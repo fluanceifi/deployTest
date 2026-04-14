2e9847faef734f3a126a1674322606d43c72410a
FluanceIFI <gksrnr66@gmail.com>
2026-04-14 11:36:14 +0900

    init

---
+++ backend/jenkins-sornar/src/main/java/test/deploy/jenkins_sornar/JenkinsSornarApplication.java
@@ -0,0 +1,13 @@
+package test.deploy.jenkins_sornar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSornarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSornarApplication.class, args);
+	}
+
+}
---
+++ backend/jenkins-sornar/src/test/java/test/deploy/jenkins_sornar/JenkinsSornarApplicationTests.java
@@ -0,0 +1,13 @@
+package test.deploy.jenkins_sornar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSornarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
